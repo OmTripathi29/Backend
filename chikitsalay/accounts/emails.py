0c4246170fea50272b688e3338a41f1259ca5ce8
SHASHWAT6969 <143774299+SHASHWAT6969@users.noreply.github.com>
2025-12-07 15:13:44 +0530

    Updated Async Email Logic

--- chikitsalay/accounts/emails.py
+++ chikitsalay/accounts/emails.py
@@ -4,18 +4,19 @@ from django.conf import settings
 from .models import User
 from django.utils import timezone
 from datetime import timedelta
+import threading
 
 def send_otp_via_email(email):
     subject = "Verify your email"
     otp=random.randint(100000, 999999)
     message = f"Your OTP for email verification is: {otp}"
     email_from = settings.EMAIL_HOST_USER
-    send_mail(subject, message, email_from,[email])
     user_obj=User.objects.get(email=email)
     user_obj.otp=otp
     user_obj.otp_created_at = timezone.now()
     user_obj.otp_attempts = 0 
     user_obj.save()
+    threading.Thread(target=send_mail, args=(subject, message, email_from, [email])).start()
     
 def is_otp_expired(user):
     if not user.otp_created_at:
@@ -28,10 +29,10 @@ def forget_password_email(email):
     otp=random.randint(100000, 999999)
     message = f"Your OTP for password reset is: {otp}"
     email_from = settings.EMAIL_HOST_USER
-    send_mail(subject, message, email_from,[email])
     user_obj=User.objects.get(email=email)
     user_obj.otp=otp
     user_obj.otp_created_at = timezone.now()
     user_obj.otp_attempts = 0 
     user_obj.save()
+    threading.Thread(target=send_mail, args=(subject, message, email_from, [email])).start()
     

bd96251958d40a4caba6b48ffd4b8f5b7f568005
SHASHWAT6969 <143774299+SHASHWAT6969@users.noreply.github.com>
2025-12-18 23:49:43 +0530

    Updated Doctors Model

--- chikitsalay/doctors/views.py
+++ chikitsalay/doctors/views.py
@@ -14,7 +14,7 @@ from .serializers import (
     DoctorSpecializationSerializer,
 )
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def doctors_view(request):
 
     if request.method == "GET":
@@ -22,6 +22,12 @@ def doctors_view(request):
         serializer = DoctorSerializer(doctors, many=True)
         return Response(serializer.data)
 
+    if request.method == "POST":
+        serializer = DoctorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 @api_view(["GET", "PATCH", "DELETE"])
 #@permission_classes([IsAuthenticated])
 def doctor_detail_view(request, doctor_id):

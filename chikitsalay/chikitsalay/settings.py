4548c3a13fec1191aab73387ca6f81b643643625
SHASHWAT6969 <143774299+SHASHWAT6969@users.noreply.github.com>
2026-07-20 15:45:08 +0530

    Made changes in email verification

--- chikitsalay/chikitsalay/settings.py
+++ chikitsalay/chikitsalay/settings.py
@@ -153,8 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER") or 'arnavvpandeyy@gmail.com'
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD") or 'eczvmfqfjymxlyyf'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_TLS=True
 
